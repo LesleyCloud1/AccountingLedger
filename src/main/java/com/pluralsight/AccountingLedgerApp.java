e2fd2e47575906e443f46e91537b887ba21c3973
Lesley Solorzano <lsolorzano@appdev.yearup.org>
2025-04-29 10:38:29 -0500

    Completed if choice 1 and 2 that ask user for deposit and payment data an saves to ouir csv file.

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,5 +1,8 @@
 package com.pluralsight;
+import java.io.File;
 import java.util.Scanner;
+import java.io.FileWriter:
+import java.io.IOException:
 
 public class AccountingLedgerApp {
     public static void main(String[] args) {
@@ -15,23 +18,71 @@ public class AccountingLedgerApp {
             System.out.println("4: Exit");
             System.out.println("Select Option #1-4: ");
 
-            String choice = scanner.nextLine().trim();
-            //trim removes extra spaces on user input
-        if (choice.equals("1")) {
-            System.out.println("Make Deposit Selected");
-        } else if (choice.equals("2")) {
-            System.out.println("Make Payment Selected");
-        } else if (choice.equals("3")) {
-            System.out.println("View Account Transactions Selected");
-        } else if (choice.equals("4")) {
-            System.out.println("Have a nice day!");
-        }else {
-            System.out.println("Invalid, select options #1-4");
+            String choice = scanner.nextLine().trim();//trim removes extra spaces on user input
 
+            if (choice.equals("1")) {
+                System.out.println("\n---- Make Deposit ----");
+                //Ask user for deposit details this info will be saved to transactions csv file
+                System.out.print("Enter date (YYYY-MM-DD): ");
+                String date = scanner.nextLine().trim();
+                System.out.print("Enter time (HH:MM:SS): ");
+                String time = scanner.nextLine().trim();
+                System.out.print("Enter Description: ");
+                String description = scanner.nextLine().trim();
+                System.out.print("Enter Vendor: ");
+                String vendor = scanner.nextLine().trim();
+                System.out.print("Enter Amount: ");
+                String amount = scanner.nextLine().trim();
+
+                //Confirm user data
+                System.out.println("\n Deposit Confirmation:");
+                System.out.println(date + "|" + time + "|" + description + "|" + vendor + "|" + amount);
+
+                //Save deposit data to transactions.csv
+                try (FileWriter = new FileWriter("transactions.csv", true)) {
+                    writer.write(date + "|" + time + "|" + description + "|" + vendor + "|" + amount + "\n");
+                    System.out.println("Deposit saved to transactions.csv");
+                } catch (IOException e) {
+                    System.out.println("Error saving deposit: " + e.getMessage());
+                }
+
+            } else if (choice.equals("2")) {
+                System.out.println("\n---- Make Payment ----"));
+                //Ask user for payment details this info will be saved to transactions csv file
+                System.out.print("Enter date (YYYY-MM-DD): ");
+                String date = scanner.nextLine().trim();
+                System.out.print("Enter time (HH:MM:SS): ");
+                String time = scanner.nextLine().trim();
+                System.out.print("Enter description: ");
+                String description = scanner.nextLine().trim();
+                System.out.print("Enter vendor: ");
+                String vendor = scanner.nextLine().trim();
+                System.out.print("Enter Amount: ");
+                String amount = scanner.nextLine().trim();
+                //Create minus sign for negative number
+                if (!amount.startsWith("-")) {
+                    amount = "-" + amount;
+                }
+                //Confirm user data
+                System.out.println("\n Payment Confirmation:");
+                System.out.println(date + "|" + time + "|" + description + "|" + vendor + "|" + amount);
+
+                //Save payment data to transactions.csv
+                try (FileWriter = new FileWriter("transactions.csv", true)) {
+                    writer.write(date + "|" + time + "|" + description + "|" + vendor + "|" + amount + "\n");
+                    System.out.println("Payment saved to transactions.csv");
+                } catch (IOException e) {
+                    System.out.println("Error saving deposit: " + e.getMessage());
+
+
+            } else if (choice.equals("3")) {
+                System.out.println("View Account Transactions Selected");
+            } else if (choice.equals("4")) {
+                System.out.println("Have a nice day!");
+                running = false; //properly exit the loop
+            } else {
+                System.out.println("Invalid, select options #1-4");
+            }
         }
-
-        }
-
     }
-
 }
